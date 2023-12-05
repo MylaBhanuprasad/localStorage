e8d06f8eb4661a916b3ed13b73ca6a1a55b3d299
MylaBhanuprasad <bhanu.myla39@gmail.com>
2023-12-05 15:33:15 +0530

    edit2

--- indexpge.js
+++ indexpge.js
@@ -1,8 +1,13 @@
 
 document.getElementById('signup').addEventListener('click',addToLocalStorage);
+
 function addToLocalStorage(){
     var uname=document.getElementById('name').value;
     var Email=document.getElementById('email').value;
-    localStorage.setItem('name',uname);
-    localStorage.setItem('email',Email);
+    let myobj={
+       username:uname,
+       email:Email
+    }
+    var myobj_serialised=JSON.stringify(myobj)
+    localStorage.setItem('myobj',myobj_serialised);
 }
